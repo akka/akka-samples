3b7e3f811953eb425afd476f15eb91a18c3bc64e
Eugene Yokota <eed3si9n@gmail.com>
2017-05-08 11:03:57 -0400

    Replace relative source URLs to GitHub links

--- docs-gen/project/AkkaSamplePlugin.scala
+++ docs-gen/project/AkkaSamplePlugin.scala
@@ -6,13 +6,16 @@ object AkkaSamplePlugin extends sbt.AutoPlugin {
   override def requires = ParadoxPlugin
   override def trigger = allRequirements
   object autoImport {
+    val baseUrl = settingKey[String]("")
     val baseProject = settingKey[String]("")
     val templateName = settingKey[String]("")
     val bodyPrefix = settingKey[String]("")
+    val bodyTransformation = settingKey[String => String]("")
   }
   import autoImport._
   import ParadoxPlugin.autoImport._
   override def projectSettings: Seq[Setting[_]] = Seq(
+    baseUrl := "https://github.com/akka/akka-samples/blob/2.5",
     crossPaths := false,
     // Copy README.md file
     sourceDirectory in (Compile, paradox) := {
@@ -20,7 +23,7 @@ object AkkaSamplePlugin extends sbt.AutoPlugin {
       val outFile = outDir / "index.md"
       val inDir = baseDirectory.value / ".." / ".." / baseProject.value
       val inFile = inDir / "README.md"
-      IO.write(outFile, bodyPrefix.value + IO.read(inFile))
+      IO.write(outFile, bodyPrefix.value + bodyTransformation.value(IO.read(inFile)))
       if ((inDir / "tutorial").exists) {
         IO.copyDirectory(inDir / "tutorial", outDir / "tutorial")
       }
@@ -31,6 +34,14 @@ object AkkaSamplePlugin extends sbt.AutoPlugin {
                      |=======================
                      |
                      |""".stripMargin,
+    bodyTransformation := { case s =>
+      val r = """\[(.+)\]\(src/(.+)\)""".r
+      val lines0 = s.split('\n').toList
+      val lines = lines0 map { line =>
+        r.replaceAllIn(line, """\[$1\]\(""" + baseUrl.value + "/" + baseProject.value + """/src/$2\)""")
+      }
+      lines.mkString("\n")
+    },
     templateName := baseProject.value.replaceAll("-sample-", "-samples-")
   )
 }
