f160e6d969ab94b453561c498c1303f87ec3ad41
Arnout Engelen <arnout@bzzt.net>
2019-05-08 15:49:11 +0200

    Add configuration and a simple main class

---
+++ akka-sample-persistence-typed-scala/src/main/scala/sample/persistence/Main.scala
@@ -0,0 +1,36 @@
+package sample.persistence
+
+import scala.concurrent.duration._
+
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.util.Timeout
+
+object Main extends App {
+implicit val timeout: Timeout = 5.seconds
+
+    case class Ack(description: String)
+
+    // First test: start a single persistent actor.
+    // TODO: start them dynamically, actually do something with responses
+    val rootBehavior = Behaviors.setup[Ack](context => {
+        val persistentActor = context.spawn(ShoppingCart.behavior("test"), "test")
+        context.ask(persistentActor)((self: ActorRef[ShoppingCart.Result]) => ShoppingCart.UpdateItem("foo", 32, self)) {
+            case r => Ack(r.toString)
+        }
+        context.ask(persistentActor)((self: ActorRef[ShoppingCart.Result]) => ShoppingCart.UpdateItem("bar", 12, self)) {
+            case r => Ack(r.toString)
+        }
+        context.ask(persistentActor)((self: ActorRef[ShoppingCart.State]) => ShoppingCart.Get(self)) {
+            case response =>
+                Ack(response.toString)
+        }
+        Behaviors.receive((ctx, ack) => {
+            println(ack.description)
+            Behaviors.same
+        })
+    })
+    
+    ActorSystem(rootBehavior, "sample-persistence")
+}
