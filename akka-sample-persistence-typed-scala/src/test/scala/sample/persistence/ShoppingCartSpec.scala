807422024c6d60ae0b7885c19121022f2121b66d
Arnout Engelen <arnout@bzzt.net>
2019-07-16 13:17:48 +0200

    Formatting

--- akka-sample-persistence-typed-scala/src/test/scala/sample/persistence/ShoppingCartSpec.scala
+++ akka-sample-persistence-typed-scala/src/test/scala/sample/persistence/ShoppingCartSpec.scala
@@ -5,23 +5,23 @@ import akka.actor.testkit.typed.scaladsl.ActorTestKit
 import org.scalatest._
 
 class AsyncTestingExampleSpec extends WordSpec with BeforeAndAfterAll with Matchers {
-    val testKit = ActorTestKit()
+  val testKit = ActorTestKit()
 
-    "The Shopping Cart" should {
-        "keep its state" in {
-            val actor = testKit.spawn(ShoppingCart.behavior("myTestPersistenceId"))
-            val probe = testKit.createTestProbe[ShoppingCart.Result]
-            actor ! ShoppingCart.UpdateItem("foo", 42, probe.ref)
-            probe.expectMessage(ShoppingCart.OK)
-            
-            testKit.stop(actor)
+  "The Shopping Cart" should {
+    "keep its state" in {
+      val actor = testKit.spawn(ShoppingCart.behavior("myTestPersistenceId"))
+      val probe = testKit.createTestProbe[ShoppingCart.Result]
+      actor ! ShoppingCart.UpdateItem("foo", 42, probe.ref)
+      probe.expectMessage(ShoppingCart.OK)
 
-            val stateProbe = testKit.createTestProbe[ShoppingCart.State]
-            val restartedActor = testKit.spawn(ShoppingCart.behavior("myTestPersistenceId"))
-            restartedActor ! ShoppingCart.Get(stateProbe.ref)
-            stateProbe.receiveMessage.items.size should be(1)
-        }
+      testKit.stop(actor)
+
+      val stateProbe = testKit.createTestProbe[ShoppingCart.State]
+      val restartedActor = testKit.spawn(ShoppingCart.behavior("myTestPersistenceId"))
+      restartedActor ! ShoppingCart.Get(stateProbe.ref)
+      stateProbe.receiveMessage.items.size should be(1)
     }
+  }
 
-    override def afterAll(): Unit = testKit.shutdownTestKit()
+  override def afterAll(): Unit = testKit.shutdownTestKit()
 }
