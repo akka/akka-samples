309be672e33d592aefdae89eb9f804888fff6380
Arnout Engelen <arnout@bzzt.net>
2019-07-16 13:03:41 +0200

    Less verbose 'ask'
    The explicit ActorRef type for UpdateItem is still unfortunate

--- akka-sample-persistence-typed-scala/src/main/scala/sample/persistence/Main.scala
+++ akka-sample-persistence-typed-scala/src/main/scala/sample/persistence/Main.scala
@@ -1,35 +1,42 @@
 package sample.persistence
 
 import scala.concurrent.duration._
+import scala.util.Success
 
 import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.AskPattern._
 import akka.actor.typed.scaladsl.Behaviors
 import akka.util.Timeout
+import akka.actor.Scheduler
 
 object Main extends App {
 implicit val timeout: Timeout = 5.seconds
 
-    case class Ack(description: String)
+    sealed trait Result
+    case object Ok extends Result
+    case class Failed(cause: Any) extends Result
 
     // First test: start a single persistent actor.
     // TODO: start them dynamically, actually do something with responses beyond printing
     // You can 'manually' test things are actually persisted by changing the commands and looking
     // at this
-    val rootBehavior = Behaviors.setup[Ack](context => {
+    val rootBehavior = Behaviors.setup[Result](context => {
         val persistentActor = context.spawn(ShoppingCart.behavior("test"), "test")
-        context.ask(persistentActor)((self: ActorRef[ShoppingCart.Result]) => ShoppingCart.UpdateItem("foo", 32, self)) {
-            case r => Ack(r.toString)
+        context.ask(persistentActor)(ShoppingCart.UpdateItem("foo", 32, _: ActorRef[ShoppingCart.Result])) {
+            case Success(ShoppingCart.OK) => Ok
+            case e => Failed(e)
         }
-        context.ask(persistentActor)((self: ActorRef[ShoppingCart.Result]) => ShoppingCart.UpdateItem("bar", 12, self)) {
-            case r => Ack(r.toString)
+        context.ask(persistentActor)(ShoppingCart.UpdateItem("bar", 12, _: ActorRef[ShoppingCart.Result])) {
+            case Success(ShoppingCart.OK) => Ok
+            case e => Failed(e)
         }
-        context.ask(persistentActor)((self: ActorRef[ShoppingCart.State]) => ShoppingCart.Get(self)) {
-            case response =>
-                Ack(response.toString)
+        context.ask(persistentActor)(ShoppingCart.Get) {
+            case Success(ShoppingCart.State(items, _)) => Ok
+            case e => Failed(e)
         }
         Behaviors.receive((ctx, ack) => {
-            println(ack.description)
+            println(ack.toString)
             Behaviors.same
         })
     })
