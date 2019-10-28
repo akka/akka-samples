55d369ac77e9483247b577a07f25c647a6cbf6af
Christopher Batey <christopher.batey@gmail.com>
2019-10-28 08:06:22 +0000

    Update diners hakking sample to latest 2.6 and style guilde

--- akka-sample-fsm-scala/README.md
+++ akka-sample-fsm-scala/README.md
@@ -1,42 +1,15 @@
 ## Finite State Machine in Actors
 
-This sample is an adaptation of [Dining Hakkers](http://www.dalnefre.com/wp/2010/08/dining-philosophers-in-humus/). It illustrates how state and behavior can be managed within an Actor with two different approaches; using `become` and using the `FSM` trait. The sample also contains an implementation of a simple redelivering actor implemented as a FSM.
-
-## Dining Hakkers with Become
-
-Open [DiningHakkersOnBecome.scala](src/main/scala/sample/become/DiningHakkersOnBecome.scala).
-
-It illustrates how current behavior can be replaced with `context.become`. Note that no `var` members are used, instead the state is encoded in the current behavior and its parameters.
-
-Start the application by typing `sbt "runMain sample.become.DiningHakkersOnBecome"`. In the log output you can see the actions of the `Hakker` actors.
-
-Read more about `become` in [the documentation](http://doc.akka.io/docs/akka/2.6/scala/actors.html#Become_Unbecome).
-
-## Dining Hakkers with FSM
-
-Open [DiningHakkersOnFsm.scala](src/main/scala/sample/fsm/DiningHakkersOnFsm.scala).
-
-It illustrates how the states and transitions can be defined with the `akka.actor.FSM` trait.
-
-Start the application by typing `sbt "runMain sample.fsm.DiningHakkersOnFsm"`. In the log output you can see the actions of the `Hakker` actors.
-
-Read more about `akka.actor.FSM` in [the documentation](http://doc.akka.io/docs/akka/2.6/scala/fsm.html).
-
-## Simple redelivering FSM
-
-Open [FsmSimpleRedelivery.scala](src/main/scala/sample/redelivery/FsmSimpleRedelivery.scala).
-
-It illustrates how you can take care of message redelivery between two or more sides. This implementation is able to process only one message at a time.
-
-Start the application by typing `sbt "runMain sample.redelivery.FsmSimpleRedelivery"`. In the log output you can see the actions of the `Requester` and the `Receiver` actors.
+This sample is an adaptation of [Dining Hakkers](http://www.dalnefre.com/wp/2010/08/dining-philosophers-in-humus/). 
+There is no explicit support for FSMs in typed as [Behaviors can represent FSMs](https://doc.akka.io/docs/akka/2.6/typed/fsm.html).
 
 ## Dining Hakkers with Akka Typed
 
-Open [DiningHakkersTyped.scala](src/main/scala/sample/typed/DiningHakkersTyped.scala).
+Open [DiningHakkersTyped.scala](src/main/scala/sample/DiningHakkers.scala).
 
 It illustrates how the behaviors and transitions can be defined with Akka Typed.
 
-Start the application by typing `sbt "runMain sample.typed.DiningHakkersTyped"`. In the log output you can see the actions of the `Hakker` actors.
+Start the application by typing `sbt "runMain sample.DiningHakkers"`. In the log output you can see the actions of the `Hakker` actors.
 
-Read more about Akka Typed in [the documentation](http://doc.akka.io/docs/akka/current/typed).
+Read more about Akka Typed in [the documentation](http://doc.akka.io/docs/akka/2.6/).
 
---
+++ akka-sample-fsm-scala/src/main/scala/sample/DiningHakkers.scala
@@ -0,0 +1,208 @@
+package sample
+
+import akka.NotUsed
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+import sample.Chopstick.Busy
+import sample.Chopstick.ChopstickAnswer
+import sample.Chopstick.ChopstickMessage
+import sample.Chopstick.Put
+import sample.Chopstick.Take
+import sample.Chopstick.Taken
+import sample.Hakker.HakkerMessage
+
+import scala.concurrent.duration._
+
+// Akka adaptation of
+// http://www.dalnefre.com/wp/2010/08/dining-philosophers-in-humus/
+
+/*
+* A Chopstick is an actor, it can be taken, and put back
+*/
+object Chopstick {
+  sealed trait ChopstickMessage
+  final case class Take(ref: ActorRef[ChopstickAnswer]) extends ChopstickMessage
+  final case class Put(ref: ActorRef[ChopstickAnswer]) extends ChopstickMessage
+
+  sealed trait ChopstickAnswer
+  final case class Taken(chopstick: ActorRef[ChopstickMessage]) extends ChopstickAnswer
+  final case class Busy(chopstick: ActorRef[ChopstickMessage]) extends ChopstickAnswer
+
+  def apply(): Behavior[ChopstickMessage] = available()
+
+  //When a Chopstick is taken by a hakker
+  //It will refuse to be taken by other hakkers
+  //But the owning hakker can put it back
+  private def takenBy(hakker: ActorRef[ChopstickAnswer]): Behavior[ChopstickMessage] = {
+    Behaviors.receive {
+      case (ctx, Take(otherHakker)) =>
+        otherHakker ! Busy(ctx.self)
+        Behaviors.same
+      case (_, Put(`hakker`)) =>
+        available()
+      case _ =>
+        // here and below it's left to be explicit about partial definition,
+        // but can be omitted when Behaviors.receivePartial is in use
+        Behaviors.unhandled
+    }
+  }
+
+  //When a Chopstick is available, it can be taken by a hakker
+  private def available(): Behavior[ChopstickMessage] = {
+    Behaviors.receive {
+      case (ctx, Take(hakker)) =>
+        hakker ! Taken(ctx.self)
+        takenBy(hakker)
+      case _ =>
+        Behaviors.unhandled
+    }
+  }
+}
+
+object Hakker {
+  sealed trait HakkerMessage
+  case object Think extends HakkerMessage
+  case object Eat extends HakkerMessage
+  final case class ChopstickAnswerAdaptor(msg: ChopstickAnswer) extends HakkerMessage
+}
+
+/*
+* A hakker is an awesome dude or dudette who either thinks about hacking or has to eat ;-)
+*/
+class Hakker(ctx: ActorContext[HakkerMessage], name: String, left: ActorRef[ChopstickMessage], right: ActorRef[ChopstickMessage]) {
+
+  import Hakker._
+
+  private val adapter = ctx.messageAdapter(ChopstickAnswerAdaptor)
+
+  val waiting: Behavior[HakkerMessage] =
+    Behaviors.receiveMessage {
+      case Think =>
+        ctx.log.info("{} starts to think", name)
+        startThinking(ctx, 5.seconds)
+      case _ =>
+        Behaviors.unhandled
+    }
+
+  //When a hakker is thinking it can become hungry
+  //and try to pick up its chopsticks and eat
+  private lazy val thinking: Behavior[HakkerMessage] = {
+    Behaviors.receiveMessage {
+      case Eat =>
+        left ! Take(adapter)
+        right ! Take(adapter)
+        hungry
+      case _ =>
+        Behaviors.unhandled
+    }
+  }
+
+  //When a hakker is hungry it tries to pick up its chopsticks and eat
+  //When it picks one up, it goes into wait for the other
+  //If the hakkers first attempt at grabbing a chopstick fails,
+  //it starts to wait for the response of the other grab
+  private lazy val hungry: Behavior[HakkerMessage] =
+    Behaviors.receiveMessage {
+      case ChopstickAnswerAdaptor(Taken(`left`)) =>
+        waitForOtherChopstick(chopstickToWaitFor = right, takenChopstick = left)
+
+      case ChopstickAnswerAdaptor(Taken(`right`)) =>
+        waitForOtherChopstick(chopstickToWaitFor = left, takenChopstick = right)
+
+      case ChopstickAnswerAdaptor(Busy(_)) =>
+        firstChopstickDenied
+      case _ =>
+        Behaviors.unhandled
+    }
+
+  //When a hakker is waiting for the last chopstick it can either obtain it
+  //and start eating, or the other chopstick was busy, and the hakker goes
+  //back to think about how he should obtain his chopsticks :-)
+  private def waitForOtherChopstick(chopstickToWaitFor: ActorRef[ChopstickMessage],
+                            takenChopstick: ActorRef[ChopstickMessage]): Behavior[HakkerMessage] = {
+    Behaviors.receiveMessage {
+      case ChopstickAnswerAdaptor(Taken(`chopstickToWaitFor`)) =>
+        ctx.log.info("{} has picked up {} and {} and starts to eat", name, left.path.name, right.path.name)
+        startEating(ctx, 5.seconds)
+
+      case ChopstickAnswerAdaptor(Busy(`chopstickToWaitFor`)) =>
+        takenChopstick ! Put(adapter)
+        startThinking(ctx, 10.milliseconds)
+      case _ =>
+        Behaviors.unhandled
+    }
+  }
+
+  //When a hakker is eating, he can decide to start to think,
+  //then he puts down his chopsticks and starts to think
+  private lazy val eating: Behavior[HakkerMessage] = {
+    Behaviors.receiveMessage {
+      case Think =>
+        ctx.log.info("{} puts down his chopsticks and starts to think", name)
+        left ! Put(adapter)
+        right ! Put(adapter)
+        startThinking(ctx, 5.seconds)
+      case _ =>
+        Behaviors.unhandled
+    }
+  }
+
+  //When the results of the other grab comes back,
+  //he needs to put it back if he got the other one.
+  //Then go back and think and try to grab the chopsticks again
+  private lazy val firstChopstickDenied: Behavior[HakkerMessage] = {
+    Behaviors.receiveMessage {
+      case ChopstickAnswerAdaptor(Taken(chopstick)) =>
+        chopstick ! Put(adapter)
+        startThinking(ctx, 10.milliseconds)
+      case ChopstickAnswerAdaptor(Busy(_)) =>
+        startThinking(ctx, 10.milliseconds)
+      case _ =>
+        Behaviors.unhandled
+    }
+  }
+
+  private def startThinking(ctx: ActorContext[HakkerMessage], duration: FiniteDuration) = {
+    Behaviors.withTimers[HakkerMessage] { timers =>
+      timers.startSingleTimer(Eat, Eat, duration)
+      thinking
+    }
+  }
+
+  private def startEating(ctx: ActorContext[HakkerMessage], duration: FiniteDuration) = {
+    Behaviors.withTimers[HakkerMessage] { timers =>
+      timers.startSingleTimer(Think, Think, duration)
+      eating
+    }
+  }
+}
+
+/*
+* Alright, here's our test-harness
+*/
+object DiningHakkers {
+
+  def mainBehavior: Behavior[NotUsed] = Behaviors.setup { context =>
+
+    //Create 5 chopsticks
+    val chopsticks = for (i <- 1 to 5) yield context.spawn(Chopstick(), "Chopstick" + i)
+
+    //Create 5 awesome hakkers and assign them their left and right chopstick
+    val hakkers = for {
+      (name, i) <- List("Ghosh", "Boner", "Klang", "Krasser", "Manie").zipWithIndex
+    } yield context.spawn(Behaviors.setup[HakkerMessage] { ctx => new Hakker(ctx, name, chopsticks(i), chopsticks((i + 1) % 5) ).waiting }, name)
+
+    //Signal all hakkers that they should start thinking, and watch the show
+    hakkers.foreach(_ ! Hakker.Think)
+
+    Behaviors.empty
+  }
+
+  def main(args: Array[String]): Unit = {
+    ActorSystem(mainBehavior, "DinningHakkers")
+  }
+}
+
