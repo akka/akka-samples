853e1b6c1949b98d03c821d22ada97ada26082ee
Enno Runne <458526+ennru@users.noreply.github.com>
2020-08-10 15:57:01 +0200

    Remove cruft

--- akka-sample-cqrs-java/src/main/java/sample/cqrs/ShoppingCartServer.java
+++ akka-sample-cqrs-java/src/main/java/sample/cqrs/ShoppingCartServer.java
@@ -1,11 +1,9 @@
 package sample.cqrs;
 
 import akka.actor.typed.ActorSystem;
-import akka.actor.typed.javadsl.Adapter;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.server.Route;
-import akka.stream.Materializer;
 
 import java.net.InetSocketAddress;
 import java.time.Duration;
@@ -14,10 +12,7 @@ import java.util.concurrent.CompletionStage;
 class ShoppingCartServer {
 
   static void startHttpServer(Route route, int httpPort, ActorSystem<?> system) {
-    // Akka HTTP still needs a classic ActorSystem to start
-    akka.actor.ActorSystem classicSystem = Adapter.toClassic(system);
-    final Http http = Http.get(classicSystem);
-    final Materializer materializer = Materializer.matFromSystem(system);
+    final Http http = Http.get(system);
 
     CompletionStage<ServerBinding> futureBinding =
       http.newServerAt("localhost", httpPort).bind(route);
